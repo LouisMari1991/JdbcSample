6666b6f4ec2d7b5f442719063d8d9331f2965c4b
Marishunxiang@gmail.com <marishunxiang@gmail.com>
2016-11-01 17:56:08 +0800

    init

---
+++ src/main/java/com/sync/jdbc/Main.java
@@ -0,0 +1,13 @@
+package com.sync.jdbc;
+
+/**
+ * Created by YH on 2016-11-01.
+ */
+public class Main {
+
+    public static void main(String[] agrs) {
+        System.out.println("你好");
+    }
+
+
+}
