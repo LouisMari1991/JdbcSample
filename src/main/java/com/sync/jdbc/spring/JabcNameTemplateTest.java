5b3f2f4f4f9ea83c9419102c71e5ed6f85780008
Marishunxiang@gmail.com <marishunxiang@gmail.com>
2016-11-11 00:45:35 +0800

    sync on 2016/11/11

---
+++ src/main/java/com/sync/jdbc/spring/JabcNameTemplateTest.java
@@ -0,0 +1,19 @@
+package com.sync.jdbc.spring;
+
+import com.sync.jdbc.JdbcUtils;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+/**
+ * Created by Administrator on 2016/11/11 0011.
+ */
+public class JabcNameTemplateTest {
+
+  static NamedParameterJdbcTemplate maned = new NamedParameterJdbcTemplate(JdbcUtils.getDataSource());
+
+  public static void main(String[] args) {
+
+  }
+
+
+
+}
---
+++ src/main/java/com/sync/jdbc/spring/SimpleJdbcTemplateTest.java
@@ -0,0 +1,12 @@
+package com.sync.jdbc.spring;
+
+/**
+ * Created by Administrator on 2016/11/11 0011.
+ */
+public class SimpleJdbcTemplateTest {
+
+  public static void main(String[] args) {
+
+  }
+
+}
