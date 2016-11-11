58cf5b745f46ae020c79a03ea20950b41c0a7181
Marishunxiang@gmail.com <marishunxiang@gmail.com>
2016-11-11 14:49:46 +0800

    2016/11/11

--- src/main/java/com/sync/jdbc/spring/JdbcNameTemplateTest.java
+++ src/main/java/com/sync/jdbc/spring/JdbcNameTemplateTest.java
@@ -6,7 +6,7 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 /**
  * Created by Administrator on 2016/11/11 0011.
  */
-public class JabcNameTemplateTest {
+public class JdbcNameTemplateTest {
 
   static NamedParameterJdbcTemplate maned = new NamedParameterJdbcTemplate(JdbcUtils.getDataSource());
 
