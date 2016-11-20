447a2b2fc75cc82c60186be4e5a2bcf56eab65d1
Marishunxiang@gmail.com <marishunxiang@gmail.com>
2016-11-20 10:32:03 +0800

    sync on 2016/11/20

---
+++ src/main/java/com/sync/introspector/Demo.java
@@ -0,0 +1,68 @@
+package com.sync.introspector;
+
+import java.beans.BeanInfo;
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2016/11/20 0020.
+ */
+public class Demo {
+
+  /**
+   * 得到Bean的所有属性
+   *
+   * @throws IntrospectionException
+   */
+  @Test
+  public void test1() throws IntrospectionException {
+    BeanInfo info = Introspector.getBeanInfo(Person.class, Object.class);// 得到Bean自己的属性
+    PropertyDescriptor[] pds = info.getPropertyDescriptors();
+    for (PropertyDescriptor pd : pds) {
+      System.out.println(pd.getName());
+    }
+  }
+
+  /**
+   * 操纵Bean的指定属性
+   *
+   * @throws IntrospectionException
+   */
+  @Test
+  public void test2() throws IntrospectionException, InvocationTargetException, IllegalAccessException {
+
+    Person p = new Person();
+
+    PropertyDescriptor pd = new PropertyDescriptor("age", Person.class);
+
+    // 得到属性的写方法
+    Method method = pd.getWriteMethod(); // setAge
+    method.invoke(p, 45);
+
+    // 获取属性的值
+    method = pd.getReadMethod();
+
+    Object age = method.invoke(p); // getAge
+
+    System.out.println(age.toString());
+  }
+
+  /**
+   * 获取当前操作的属性类型
+   */
+  @Test
+  public void test3() throws IntrospectionException {
+
+    Person p = new Person();
+
+    PropertyDescriptor pd = new PropertyDescriptor("age",Person.class);
+
+    System.out.println(pd.getPropertyType());
+
+  }
+
+}
---
+++ src/main/java/com/sync/introspector/Person.java
@@ -0,0 +1,35 @@
+package com.sync.introspector;
+
+/**
+ * Created by Administrator on 2016/11/20 0020.
+ */
+public class Person {
+
+  private String name;
+  private String password;
+  private int age;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+}
