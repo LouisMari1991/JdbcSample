a9040feebe7180a3c2fd34242a757f17836d97bd
Marishunxiang@gmail.com <marishunxiang@gmail.com>
2016-11-20 11:23:39 +0800

    sync on 2016/11/20

--- src/main/java/com/sync/beanutils/Demo1.java
+++ src/main/java/com/sync/beanutils/Demo1.java
@@ -4,10 +4,13 @@ import java.lang.reflect.InvocationTargetException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import org.apache.commons.beanutils.BeanUtils;
 import org.apache.commons.beanutils.ConversionException;
 import org.apache.commons.beanutils.ConvertUtils;
 import org.apache.commons.beanutils.Converter;
+import org.apache.commons.beanutils.locale.converters.DateLocaleConverter;
 import org.junit.Test;
 import org.springframework.util.StringUtils;
 
@@ -24,12 +27,18 @@ public class Demo1 {
     System.out.println(p.getName());
   }
 
+  /**
+   * BeanUtils默认只支持基本数据类型转换
+   *
+   * @throws InvocationTargetException
+   * @throws IllegalAccessException
+   */
   @Test
   public void test2() throws InvocationTargetException, IllegalAccessException {
     String name = "lisi";
     String password = "123";
     String age = "20";
-    String birthday = "1980-11-11";
+    String birthday = "";
 
     Person p = new Person();
     BeanUtils.setProperty(p, "name", name);
@@ -57,6 +66,9 @@ public class Demo1 {
           throw new ConversionException("只支持String类型的转换");
         }
         String str = (String) value;
+        if (str.trim().equals("")) {
+          return null;
+        }
         SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
         try {
           return df.parse(str);
@@ -70,8 +82,42 @@ public class Demo1 {
     BeanUtils.setProperty(p, "name", name);
     BeanUtils.setProperty(p, "password", password);
     BeanUtils.setProperty(p, "age", age);// 默认只支持8中基本数据类型
-    BeanUtils.setProperty(p,"birthday",birthday); // org.apache.commons.beanutils.converters.DateConverter toDate
+    BeanUtils.setProperty(p, "birthday", birthday); // org.apache.commons.beanutils.converters.DateConverter toDate
 
     System.out.println(p);
   }
+
+  @Test
+  public void test4() throws InvocationTargetException, IllegalAccessException {
+    String name = "lisi";
+    String password = "123";
+    String age = "20";
+    String birthday = "";
+
+    ConvertUtils.register(new DateLocaleConverter(), Date.class);
+    Person p = new Person();
+
+    BeanUtils.setProperty(p, "name", name);
+    BeanUtils.setProperty(p, "password", password);
+    BeanUtils.setProperty(p, "age", age);// 默认只支持8中基本数据类型
+    BeanUtils.setProperty(p, "birthday", birthday); // org.apache.commons.beanutils.converters.DateConverter toDate
+
+    System.out.println(p);
+  }
+
+  @Test
+  public void test5() throws InvocationTargetException, IllegalAccessException {
+    Map<String, Object> map = new HashMap<String, Object>();
+    map.put("name", "wangwu");
+    map.put("password", "321");
+    map.put("age",15);
+    map.put("birthday","1980-11-11");
+
+    ConvertUtils.register(new DateLocaleConverter(), Date.class);
+    Person p = new Person();
+    BeanUtils.populate(p, map);
+
+    System.out.println(p);
+  }
+
 }
